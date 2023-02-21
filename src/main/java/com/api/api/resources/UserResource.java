56ca8a2093932951b3d536d339f91fd1b4333eb8
Carlos Henrique <carlosh.sales@outlook.com>
2023-02-21 12:55:59 -0300

    feat: initialize user resource

---
+++ src/main/java/com/api/api/resources/UserResource.java
@@ -0,0 +1,12 @@
+package com.api.api.resources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/users")
+public class UserResource {
+
+}
