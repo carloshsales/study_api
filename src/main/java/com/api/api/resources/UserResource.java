3ec5ee0797a27f9c9e7550ba9c236e567593a1eb
Carlos Henrique <carlosh.sales@outlook.com>
2023-02-21 15:26:27 -0300

    feat: find by id method

--- src/main/java/com/api/api/resources/UserResource.java
+++ src/main/java/com/api/api/resources/UserResource.java
@@ -1,5 +1,11 @@
 package com.api.api.resources;
 
+import com.api.api.domain.user.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,5 +14,14 @@ import java.util.UUID;
 @RestController
 @RequestMapping("/users")
 public class UserResource {
+
+    @Autowired
+    private FindUserByIdService findByIdService;
+
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> findById(@PathVariable UUID id){
+        return ResponseEntity.status(HttpStatus.OK).body(findByIdService.execute(id));
+    }
 
 }
