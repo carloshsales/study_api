51e778ee4ffc56e9b67f28dfd4bd65c67c4257aa
Carlos Henrique <carlosh.sales@outlook.com>
2023-02-21 12:58:04 -0300

    fix: change user classk

--- src/main/java/com/api/api/domain/user/User.java
+++ src/main/java/com/api/api/domain/user/User.java
@@ -3,22 +3,23 @@ package com.api.api.domain.user;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.math.BigInteger;
 import java.util.UUID;
 
-@Setter @Getter
+@Getter @Setter
 @NoArgsConstructor @AllArgsConstructor
-@EqualsAndHashCode
-@ToString
+@EqualsAndHashCode(exclude = {"password"})
+@ToString(exclude = {"password"})
 @Entity
+@Table(name = "td_user")
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
     private String name;
+
     @Column(unique = true)
     private String email;
-
-    @EqualsAndHashCode.Exclude
     private String password;
 }
