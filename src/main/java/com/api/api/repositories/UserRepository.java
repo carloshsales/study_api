af35041f00ef5fee57de0af6192cc213394a9565
Carlos Henrique <carlosh.sales@outlook.com>
2023-02-21 21:19:21 -0300

    feat: user repository

---
+++ src/main/java/com/api/api/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.api.api.repositories;
+
+import com.api.api.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+}
