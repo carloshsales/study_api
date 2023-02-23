870159360fbe64c38d5f10ce194032c423b2dfcc
Carlos Henrique <carlosh.sales@outlook.com>
2023-02-22 21:06:16 -0300

    feat: insert user service interface

--- src/main/java/com/api/api/services/interfaces/IUserService.java
+++ src/main/java/com/api/api/services/interfaces/IUserService.java
@@ -1,13 +1,17 @@
 package com.api.api.services.interfaces;
 
+import com.api.api.domain.dto.UserDTO;
 import com.api.api.domain.user.User;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.UUID;
 
 @Service
 public interface IUserService {
-
     User findById(UUID id);
+    List<User> findAll();
+
+    User insert(UserDTO user);
 
 }
