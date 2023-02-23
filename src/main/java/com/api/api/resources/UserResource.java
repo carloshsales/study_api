614200432a0cbf08dc400fa4e5a59f78b12a2a05
Carlos Henrique <carlosh.sales@outlook.com>
2023-02-22 21:05:17 -0300

    feat: insert database user controller

--- src/main/java/com/api/api/resources/UserResource.java
+++ src/main/java/com/api/api/resources/UserResource.java
@@ -2,36 +2,50 @@ package com.api.api.resources;
 
 import com.api.api.domain.dto.UserDTO;
 import com.api.api.domain.user.User;
-import com.api.api.services.UserService;
+import com.api.api.services.interfaces.IUserService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/users")
 public class UserResource {
 
+    private final String ID = "/{id}";
+
     @Autowired
     private ModelMapper mapper;
 
     @Autowired
-    UserService service;
+    IUserService service;
 
     @GetMapping("/")
-    public ResponseEntity<List<User>> getAll(){
-        return ResponseEntity.status(HttpStatus.OK).body(this.service.getAll());
+    public ResponseEntity<List<UserDTO>> findAll(){
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(service.findAll()
+                        .stream()
+                        .map(x -> mapper.map(x, UserDTO.class))
+                        .collect(Collectors.toList()));
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<UserDTO> findById(@PathVariable UUID id){
         return ResponseEntity.status(HttpStatus.OK).body(mapper.map(service.findById(id), UserDTO.class));
+    }
+
+    @PostMapping("/")
+    public ResponseEntity<UserDTO> insert(@RequestBody UserDTO userDTO){
+        URI uri = ServletUriComponentsBuilder
+                .fromCurrentRequest().path(ID).buildAndExpand(service.insert(userDTO).getId()).toUri();
+        return ResponseEntity.created(uri).build();
     }
 }
