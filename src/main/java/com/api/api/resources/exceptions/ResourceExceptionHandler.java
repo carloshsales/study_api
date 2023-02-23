6ff35ec33a0722e186b21890557e1552522bc407
Carlos Henrique <carlosh.sales@outlook.com>
2023-02-23 00:36:03 -0300

    feat: data integrity violation exception

--- src/main/java/com/api/api/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/api/api/resources/exceptions/ResourceExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.api.api.resources.exceptions;
 
+import com.api.api.services.exceptions.DataIntegrityViolationException;
 import com.api.api.services.exceptions.ObjectNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpRequest;
@@ -14,7 +15,7 @@ import java.time.LocalDateTime;
 public class ResourceExceptionHandler {
 
     @ExceptionHandler(ObjectNotFoundException.class)
-    public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException exception, HttpServletRequest request){
+    public ResponseEntity<StandardError> objectNotFoundException(ObjectNotFoundException exception, HttpServletRequest request){
         StandardError error = new StandardError(
                 LocalDateTime.now(),
                 HttpStatus.NOT_FOUND.value(),
@@ -23,5 +24,17 @@ public class ResourceExceptionHandler {
         );
 
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<StandardError> dataIntegrityViolationException(DataIntegrityViolationException exception, HttpServletRequest request){
+        StandardError error = new StandardError(
+                LocalDateTime.now(),
+                HttpStatus.BAD_REQUEST.value(),
+                exception.getMessage(),
+                request.getRequestURI()
+        );
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
     }
 }
---
+++ src/main/java/com/api/api/services/exceptions/DataIntegrityViolationException.java
@@ -0,0 +1,7 @@
+package com.api.api.services.exceptions;
+
+public class DataIntegrityViolationException extends RuntimeException{
+    public DataIntegrityViolationException(String message){
+        super(message);
+    }
+}
