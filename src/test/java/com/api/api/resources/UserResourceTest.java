051061290ff901f1887f3f9cb724909341efcf7d
Carlos Henrique <carlosh.sales@outlook.com>
2023-02-24 18:03:59 -0300

    feat: resource test class

---
+++ src/test/java/com/api/api/resources/UserResourceTest.java
@@ -0,0 +1,33 @@
+package com.api.api.resources;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserResourceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void insert() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+}
